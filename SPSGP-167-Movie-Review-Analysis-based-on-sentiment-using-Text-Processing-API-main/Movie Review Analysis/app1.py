013be466d863f5904775d9ff54407cf3ef084c9f
mukesh736 <80209216+mukesh736@users.noreply.github.com>
2021-03-08 10:41:00 +0530

    Add files via upload

---
+++ SPSGP-167-Movie-Review-Analysis-based-on-sentiment-using-Text-Processing-API-main/Movie Review Analysis/app1.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template
+import numpy as np
+import re
+import requests
+import json
+
+app = Flask(__name__)
+
+def check(output):
+    url =  "https://japerk-text-processing.p.rapidapi.com/sentiment/"
+    payload = {"text": output}
+    print(payload)
+    headers = {
+    'content-type': "application/x-www-form-urlencoded",
+    'x-rapidapi-key': "365608f06amsh59ba7850bc77db5p11cdfcjsn4395ecae216f",
+    'x-rapidapi-host': "japerk-text-processing.p.rapidapi.com"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+    value = response.text
+    output=json.loads(value)
+    return response.json()
+#home page
+@app.route('/')
+def summarizer():
+    return render_template('summarizer.html')
+
+#summarizer page
+@app.route('/summarize',  methods=['POST'])
+def summarize():
+    output = request.form['output']
+    output=re.sub("[^a-zA-Z.,]"," ",output)
+    print(output)
+    essay = check(output)
+    print(type(essay['label']))
+    if essay['label'] == "pos":
+        output="Positive review"
+    elif essay['label'] == "neg":
+        output="Negative review"
+    else:
+        output="Neutral Review"
+    #print(max(essay['probability'], key=essay.get))
+    return render_template('summary.html',essay=essay,prediction_text='{}'.format(output))
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ SPSGP-167-Movie-Review-Analysis-based-on-sentiment-using-Text-Processing-API-main/README.md
@@ -0,0 +1,13 @@
+# SPSGP-167-Movie-Review-Analysis-based-on-sentiment-using-Text-Processing-API
+Movie Review Analysis based on  sentiment using Text-Processing API
+
+B-1
+CSE - D
+
+Team members :
+
+Shivani Naik
+
+V. Pavithra
+
+R.likitha
